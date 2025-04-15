1792b42f0870b9ce3ae7b37da5f2385375331475
ali_khanix <khaniali568@gmail.com>
2025-04-15 16:27:17 +0330

    services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,47 @@
+"use client";
+
+import { ArrowDownRight } from "lucide-react";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur.",
+    href: "",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Services = () => {
-  return <div>Services Page</div>;
+  return (
+    <section>
+      <div className="container mx-auto">Services Page</div>
+    </section>
+  );
 };
 
 export default Services;
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -22,7 +22,7 @@ const stats = [
 ];
 const Stats = () => {
   return (
-    <section className="">
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
@@ -38,7 +38,13 @@ const Stats = () => {
                   className="text-4xl xl:text-6xl font-extrabold"
                 />
 
-                <p>{item.text}</p>
+                <p
+                  className={`${
+                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
+                >
+                  {item.text}
+                </p>
               </div>
             );
           })}
