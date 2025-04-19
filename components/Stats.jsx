edab2bef58d7c9686e7fbb42b2b9c53c7e8c335d
ali-khanix <khaniali568@gmail.com>
2025-04-19 05:35:20 +0330

    fixed stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -38,7 +38,7 @@ const Stats = () => {
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                className="flex-1 flex gap-4 items-center justify-start"
                 key={index}
               >
                 <CountUp
